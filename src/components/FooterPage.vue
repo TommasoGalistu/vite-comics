a84d46e3c0c467a2b4c7eb6f64b98f9cd04b7589
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-05 17:48:13 +0200

    feat: first photo in container

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,17 +1,20 @@
 <script>
 import FinalPartFooter from "./footerComponent/FinalPartFooter.vue";
 import PolicyPart from "./footerComponent/PolicyPart.vue";
+import UpPartFooter from "./footerComponent/UpPartFooter.vue";
 export default {
   name: "FooterPage",
   components: {
     FinalPartFooter,
     PolicyPart,
+    UpPartFooter,
   },
 };
 </script>
 
 <template>
   <h1>Footer</h1>
+  <UpPartFooter />
   <PolicyPart />
   <FinalPartFooter />
 </template>
---
+++ src/components/footerComponent/UpPartFooter.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "UpPartFooter",
+};
+</script>
+
+<template>
+  <div class="containerAll">
+    <div class="mainContainer">
+      <div class="contImg">
+        <img
+          src="/src/assets/ecommercePhoto/buy-comics-digital-comics.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../style/partials/_mixins.scss" as *;
+@use "../../style/partials/variables.scss" as *;
+
+.containerAll {
+  background-color: #0f82f7;
+
+  .contImg {
+    width: 3rem;
+  }
+}
+</style>
