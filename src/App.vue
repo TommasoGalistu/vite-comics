111566702ebef57670438831af424afe95bd1cf7
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-05 14:49:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import HeaderPage from "../src/components/Header.vue";
+export default {
+  name: "App",
+  HeaderPage,
+};
+</script>
+
+<template>
+  <HeaderPage />
+</template>
+
+<style lang="scss" >
+@import url("./style/general.scss");
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "HeaderPage",
+};
+</script>
+
+<template>
+  <h1>ciao</h1>
+</template>
+
+<style>
+</style>
