f94c50a9df68f1d892b74e17524a0309a6c8a532
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-05 15:49:49 +0200

    feat: first component footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,11 +1,16 @@
 <script>
+import FinalPartFooter from "./footerComponent/FinalPartFooter.vue";
 export default {
   name: "FooterPage",
+  components: {
+    FinalPartFooter,
+  },
 };
 </script>
 
 <template>
   <h1>Footer</h1>
+  <FinalPartFooter />
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/footerComponent/FinalPartFooter.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "FinalPartFooter",
+};
+</script>
+
+<template>
+  <div class="mainContainer">
+    <button>SIGN-UP NOW!</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
