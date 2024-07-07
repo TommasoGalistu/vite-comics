1f539f612b11ba503d28ba252b04286dfd08b5be
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-07 12:26:39 +0200

    fix: data in extra file

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,64 +1,10 @@
 <script>
+import { links } from "../data/menuHeader";
 export default {
   name: "HeaderPage",
   data() {
     return {
-      links: [
-        {
-          text: "CHARACTERS",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "COMICS",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "MOVIES",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "TV",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "GAMES",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "COLLECTIBLES",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "VIDEOS",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "FANS",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "NEWS",
-          url: "#",
-          current: false,
-        },
-        {
-          text: "SHOP",
-          url: "#",
-          current: false,
-        },
-      ],
-      linkAttivato: {
-        index: "",
-        boolValue: false,
-      },
+      links,
     };
   },
   methods: {
